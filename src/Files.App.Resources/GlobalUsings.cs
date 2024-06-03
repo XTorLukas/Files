891f415f78f1e12235073cd898df579c9b71b9bb
Lukáš Laštůvka <lastuvka.lukas3@gmail.com>
2024-06-03 20:18:11 +0200

    Added project for managing localizations

---
+++ src/Files.App.Resources/GlobalUsings.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) 2024 Files Community
+// Licensed under the MIT License. See the LICENSE.
+
+global using global::Jeffijoe.MessageFormat;
+global using global::System;
+global using global::System.Collections.Generic;
+global using global::System.Globalization;
+global using global::System.IO;
+global using global::System.Linq;
+global using global::System.Reflection;
+global using global::YamlDotNet.Serialization;
+global using global::YamlDotNet.Serialization.NamingConventions;
