30c00148c3ccf25de02fdcd4597b3cc4514aae18
Lukáš Laštůvka <lastuvka.lukas3@gmail.com>
2024-07-05 13:00:16 +0200

    Add Strings real-time binder

---
+++ src/Files.App/Extensions/StringsExtensions.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) 2024 Files Community
+// Licensed under the MIT License. See the LICENSE.
+
+namespace Files.App.Extensions
+{
+	public static class StringsExtensions
+	{
+		private static readonly IResourceManager _manager = Ioc.Default.GetRequiredService<IResourceManager>();
+
+		public static string ToLocalized_NEW(this string key, List<object>? args = null)
+		{
+			return args == null
+				? new Strings() { KeyValue = key }.ToLocalized()
+				: new Strings() { KeyValue = key, ArgsValue = args }.ToLocalized();
+		}
+
+		public static void ToLocalized(this Strings dataValue, string key, List<object>? args = null)
+		{
+			if (dataValue is null || (dataValue.KeyValue == key &&
+				((dataValue.IsArgsNull && args is null) ||
+				(!dataValue.IsArgsNull && args is not null && dataValue.ArgsValue!.SequenceEqual(args)))))
+				return;
+
+			dataValue.KeyValue = key;
+			dataValue.ArgsValue = args;
+
+			_ = _manager.RealTimeService.UpdateDataValueProvider(dataValue);
+		}
+	}
+}
---
+++ src/Files.App/Helpers/StringsHelper.cs
@@ -0,0 +1,55 @@
+﻿// Copyright (c) 2024 Files Community
+// Licensed under the MIT License. See the LICENSE.
+
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Markup;
+
+namespace Files.App.Helpers
+{
+	[MarkupExtensionReturnType(ReturnType = typeof(string))]
+	public sealed partial class Strings : MarkupExtension, IRealTimeDataValueManager
+	{
+		private static readonly IResourceManager _manager = Ioc.Default.GetRequiredService<IResourceManager>();
+
+		public string KeyValue { get; set; } = string.Empty;
+
+		private List<object>? _argsValue = null;
+
+		[EditorBrowsable(EditorBrowsableState.Never)]
+		public List<object>? ArgsValue
+		{
+			get => _argsValue ??= [];
+			set => _argsValue = value;
+		}
+
+		public bool IsArgsNull => _argsValue is null;
+
+		public string ToLocalized()
+		{
+			return IsArgsNull
+				? _manager.GetString(KeyValue)
+				: string.Format(_manager.GetString(KeyValue), ArgsValue!.ToArray());
+		}
+
+		protected override string ProvideValue(IXamlServiceProvider serviceProvider)
+		{
+			if (RealTimeEnable && serviceProvider.GetService(typeof(IProvideValueTarget)) is IProvideValueTarget service)
+				_ = _manager.RealTimeService.RegisterDataValueProvider(service, this);
+
+			return ToLocalized();
+		}
+
+		[EditorBrowsable(EditorBrowsableState.Never)]
+		public ProvideValueTargetProperty? RealTimeProperty { get; set; }
+
+		[EditorBrowsable(EditorBrowsableState.Never)]
+		public DependencyObject? RealTimeTarget { get; set; }
+
+		public bool RealTimeEnable { get; set; } = true;
+
+		[EditorBrowsable(EditorBrowsableState.Never)]
+		public Func<string> RealTimeValueProvider => ToLocalized;
+
+		~Strings() => _manager.RealTimeService.UnregisterDataValueProvider(this);
+	}
+}
